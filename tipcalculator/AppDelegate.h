6bd203afbfe58130f19e9d6fbb69a80330d40078
David Law <daklaw@gmail.com>
2014-02-19 00:54:08 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by David Law on 2/19/14.
+//  Copyright (c) 2014 Stephen Hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
